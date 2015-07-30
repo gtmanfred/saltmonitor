fa81deae30d6aae882d021b059d6700d23670e16
Daniel Wallace <danielwallace@gtmanfred.com>
2015-07-30 14:52:40 -0500

    only initialize the pool once
    Yay base class

--- saltmonitor/api/resources.py
+++ saltmonitor/api/resources.py
@@ -3,39 +3,47 @@ from flask.ext.restful import Resource
 from pymongo import MongoClient
 
 # connect=False for https://jira.mongodb.org/browse/PYTHON-961
-client = MongoClient('mongodb://localhost:27017/', connect=False)
-collection = client.salt
+
+class BaseResource(Resource):
+    client = None
+    def __new__(cls, *args, **kwargs):
+        if BaseResource.client is None:
+            BaseResource.client = MongoClient('mongodb://localhost:27017/', connect=False)
+            BaseResource.collection = BaseResource.client.salt
+        object.__new__(cls)
+        cls.collection = BaseResource.collection
+        return super(BaseResource, cls).__new__(cls)
 
 
-class Jobs(Resource):
+class Jobs(BaseResource):
     def get(self):
-        jobs = collection.jobs
+        jobs = self.collection.jobs
         ret = list(jobs.find().sort('jid', -1))
         for x in ret:
             x['_id'] = str(x['_id'])
         return jsonify({'jobs': ret})
 
 
-class Job(Resource):
+class Job(BaseResource):
     def get(self, jid):
-        jobs = collection.jobs
+        jobs = self.collection.jobs
         ret = jobs.find_one({'jid': jid})
         ret['_id'] = str(ret['_id'])
         return jsonify(ret)
 
 
-class SaltReturns(Resource):
+class SaltReturns(BaseResource):
     def get(self):
-        returns = collection.saltReturns
+        returns = self.collection.saltReturns
         ret = list(returns.find())
         for x in ret:
             x['_id'] = str(x['_id'])
         return jsonify({'results': ret})
 
 
-class SaltReturn(Resource):
+class SaltReturn(BaseResource):
     def get(self, jid):
-        returns = collection.saltReturns
+        returns = self.collection.saltReturns
         ret = list(returns.find({'jid': jid}).sort('_id'))
         for x in ret:
             x['_id'] = str(x['_id'])
